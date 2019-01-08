e54b260a4495fb7e60959f782596a37708eebf08
Zjinxing <1182999456@qq.com>
2019-01-08 19:55:42 +0800

    wechatAPI

--- routes/wechatAPI.js
+++ routes/wechatAPI.js
@@ -303,26 +303,270 @@ router.post('/massSendText', async ctx => {
   }
 })
 
-/*************** 素材管理 *************/
-
-// 上传图片
-
-router.post('/uploadImageMaterial', async ctx => {
-  const file = ctx.request.files.file
-  console.log(file)
-  const mypath = path.resolve(`${file.name}`)
-  console.log(mypath)
+// 群发声音消息
+router.post('/massSendVoice', async ctx => {
+  const { body } = ctx.request
+  const { mediaId, receivers } = body
   try {
-    ctx.body = await api.uploadImageMaterial(mypath)
+    ctx.body = await api.massSendVoice(mediaId, receivers)
   } catch (err) {
     ctx.body = err
   }
 })
 
-// upload Test
-router.post('/upload', ctx => {
-  const file = ctx.request.files.file
-  console.log(file)
+// 群发图片消息
+router.post('/massSendImage', async ctx => {
+  const { body } = ctx.request
+  const { mediaId, receivers } = body
+  try {
+    ctx.body = await api.massSendImage(mediaId, receivers)
+  } catch (err) {
+    ctx.body = err
+  }
 })
+
+// 群发视频消息
+router.post('/massSendVideo', async ctx => {
+  const { body } = ctx.request
+  const { mediaId, receivers } = body
+  try {
+    ctx.body = await api.massSendVideo(mediaId, receivers)
+  } catch (err) {
+    ctx.body = err
+  }    
+})
+
+// 删除群发消息
+router.post('/deleteMass', async ctx => {
+  const { body } = ctx.request
+  const { message_id } = body
+  try {
+    ctx.body = await api.deleteMass(message_id)
+  } catch (err) {
+    ctx.body = err
+  }    
+})
+
+// 预览图文消息
+router.post('/previewNews', async ctx => {
+  const { body } = ctx.request
+  const { openid, mediaId } = body
+  try {
+    ctx.body = await api.previewNews(openid, mediaId)
+  } catch (err) {
+    ctx.body = err
+  }    
+})
+
+// 预览声音消息
+router.post('/previewVoice', async ctx => {
+  const { body } = ctx.request
+  const { openid, mediaId } = body
+  try {
+    ctx.body = await api.previewVoice(openid, mediaId)
+  } catch (err) {
+    ctx.body = err
+  }    
+})
+
+// 预览图片消息
+router.post('/previewImage', async ctx => {
+  const { body } = ctx.request
+  const { openid, mediaId } = body
+  try {
+    ctx.body = await api.previewImage(openid, mediaId)
+  } catch (err) {
+    ctx.body = err
+  }    
+})
+
+// 预览视频消息
+router.post('/previewVideo', async ctx => {
+  const { body } = ctx.request
+  const { openid, mediaId } = body
+  try {
+    ctx.body = await api.previewVideo(openid, mediaId)
+  } catch (err) {
+    ctx.body = err
+  }    
+})
+
+// 预览文本消息
+router.post('/previewText', async ctx => {
+  const { body } = ctx.request
+  const { openid, content } = body
+  try {
+    ctx.body = await api.previewText(openid, content)
+  } catch (err) {
+    ctx.body = err
+  }    
+})
+
+/*************** 模版消息 **************/
+
+// 设置所属行业
+router.post('/setIndustry', async ctx => {
+  const { body } = ctx.request
+  const { industry } = body
+  try {
+    ctx.body = await api.setIndustry(industry)
+  } catch (err) {
+    ctx.body = err
+  }
+})
+
+// 获得模版id
+router.post('/addTemplate', async ctx => {
+  const { body } = ctx.request
+  const { templateIdShort } = body
+  try {
+    ctx.body = await api.addtemplate(templateIdShort)
+  } catch (err) {
+    ctx.body = err
+  }
+})
+
+// 发送模版消息
+router.post('/sendTemplate', async ctx => {
+  const { body } = ctx.request
+  const { openid, templateId, url, topColor, data } = body
+  try {
+    ctx.body = await api.sendTemplate(openid, templateId, url, topColor, data)
+  } catch (err) {
+    ctx.body = err
+  }
+})
+
+
+/*************** 素材管理 *************/
+
+// 上传永久素材
+router.post('/uploadMaterial', async ctx => {
+  const file = ctx.request.files.file
+  const reader = fs.createReadStream(file.path)
+  let filepath = path.resolve(file.name)
+  const type = file.type
+  const upStream = fs.createWriteStream(filepath);
+  reader.pipe(upStream)
+  // console.log('文件路径',filepath)
+  try {
+    ctx.body = await api.uploadMaterial(filepath, type)
+  } catch (err) {
+    ctx.body = err
+  }
+})
+
+// 上传图片
+router.post('/uploadImageMaterial', async ctx => {
+  const file = ctx.request.files.file
+  const reader = fs.createReadStream(file.path)
+  let filepath = path.resolve(file.name)
+  const upStream = fs.createWriteStream(filepath);
+  reader.pipe(upStream)
+  // console.log('文件路径',filepath)
+  try {
+    ctx.body = await api.uploadThumbMaterial(filepath)
+  } catch (err) {
+    ctx.body = err
+  }
+})
+
+// 上传声音素材
+router.post('/uploadVoiceMaterial', async ctx => {
+  const file = ctx.request.files.file
+  const reader = fs.createReadStream(file.path)
+  let filepath = path.resolve(file.name)
+  const upStream = fs.createWriteStream(filepath);
+  reader.pipe(upStream)
+  try {
+    ctx.body = await api.uploadThumbMaterial(filepath)
+  } catch (err) {
+    ctx.body = err
+  }
+})
+
+// 上传缩略图
+router.post('/uploadThumbMaterial', async ctx => {
+  const file = ctx.request.files.file
+  const reader = fs.createReadStream(file.path)
+  let filepath = path.resolve(file.name)
+  const upStream = fs.createWriteStream(filepath);
+  reader.pipe(upStream)
+  try {
+    ctx.body = await api.uploadThumbMaterial(filepath)
+  } catch (err) {
+    ctx.body = err
+  }
+})
+
+// 上传视频永久素材
+router.post('/uploadVideoMaterial', async ctx => {
+  const { body } = ctx.request
+  const { description } = body
+  const file = ctx.request.files.file
+  const reader = fs.createReadStream(file.path)
+  const filepath = path.resolve(file.name)
+  const upStream = fs.createWriteStream(filepath)
+  reader.pipe(upStream)
+  try {
+    ctx.body = await api.uploadVideoMaterial(filepath, description)
+  } catch (err) {
+    ctx.body = err
+  }
+})
+
+// 上传图文永久素材
+router.post('/uploadNewsMaterial', async ctx => {
+  const { body } = ctx.request
+  const { news } = body
+  try {
+    ctx.body = await api.uploadNewsMaterial(news)
+  } catch (err) {
+    ctx.body = err
+  }
+})
+
+// 获取永久素材
+router.post('/getMaterial', async ctx => {
+  const { body } = ctx.request
+  const { mediaId } = body
+  try {
+    ctx.body = await api.getMaterial(mediaId)
+  } catch (err) {
+    ctx.body = err
+  }
+})
+
+// 删除永久素材
+router.post('/removeMaterial', async ctx => {
+  const { body } = ctx.request
+  const { mediaId } = body
+  try {
+    ctx.body = await api.removeMaterial(mediaId)
+  } catch (err) {
+    ctx.body = err
+  }
+})
+
+// 获取素材总数
+router.get('/getMaterialCount', async ctx => { 
+  try {
+    ctx.body = await api.getMaterialCount()
+  } catch (err) {
+    ctx.body = err
+  }
+})
+
+// 获取素材列表
+router.post('/getMaterials', async ctx => {
+  const { body } = ctx.request
+  const { type, offset, count } = body
+  try {
+    ctx.body = await api.getMaterials(type, offset, count)
+  } catch (err) {
+    ctx.body = err
+  }
+})
+
 
 module.exports = router
